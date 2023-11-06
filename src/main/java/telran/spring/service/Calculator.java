0100fc30c3b3110f32d672d17afa7f3af1ee2508
Georgy Levin <g.phyzy@gmail.com>
2023-11-06 17:40:01 +0200

    HW 56 fixed: Updates package name for service

--- src/main/java/telran/spring/service/Calculator.java
+++
@@ -1,9 +0,0 @@
-package telran.spring.service;
-
-public interface Calculator {
-	double multiply(double op1, double op2);
-	double sum(double op1, double op2);
-	double substact(double op1, double op2);
-	double divide(double op1, double op2);
-
-}
